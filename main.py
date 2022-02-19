b48403b7220b288ea342021fd9798f2eb0b8f54d
Maycon Coutinho <60453269+MayconCoutinho@users.noreply.github.com>
2022-02-19 17:44:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ def alinha():
     from pyautogui import locateOnScreen, click    
 
 
-    button_pos = locateOnScreen('./Image/boneco.png', confidence=0.80)
-
+    button_pos = locateOnScreen('./Image/boneco.png', confidence=0.80)         #  "locateOnScreen" com base na imagem posta, localiza na tela padrão de pixel semelhante a imagem,
+                                                                               #  "confidence=0.80" é a quantidade de "certeza" de que a imagem base é igual a imagem da tela.
     button_pos2 = locateOnScreen('./Image/boneco2.png', confidence=0.80)
             
     if button_pos != None:
 
-        click(button_pos.left+65,button_pos.top+110) # Se adicionar button="RIGHT" o botão do mouse direito e clicador 
+        click(button_pos.left+65,button_pos.top+110)                           # "click" tem como usar para clicar, mas apenas ta movendo o mouse       
     
         return True
 
     if button_pos2 != None:
 
-        click(button_pos2.left+65,button_pos2.top+110) # Se adicionar button="RIGHT" o botão do mouse direito e clicador 
+        click(button_pos2.left+65,button_pos2.top+110) 
 
         return True
 
@@ -29,7 +29,7 @@ while True:
     from time import sleep
     
 
-    if is_pressed('s') and ligado == False:
+    if is_pressed('s') and ligado == False:         # "is_pressed()" indentifica se a tecla esta sendo pressionada.
         ligado = True
         print('Ta ligado')
         sleep(1)
